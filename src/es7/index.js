0cccaabf210d66fa1ea9e50f454e1eabe90c4be0
Carlos Interiano <carinthnd@gmail.com>
2021-05-28 20:38:20 -0600

    Cambios en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+// INCLUDES
+// ------------------------------------------------------
+let numbers = [1, 2, 3, 4, 8]
+
+if (numbers.includes(8)) {
+    console.log('Si se encuentra el valor')
+} else {
+    console.log('No se encuentra')
+}
+
+// POTENCIA
+// ------------------------------------------------------
+let base = 4
+let exponent = 3
+let result = base ** exponent
+
+console.log(result)
+
